8eb924edb6da9b33916473c90b5462aaf671e27b
Mighty-SEA <hibban.habiburrahman@outlook.com>
2025-07-03 15:27:14 +0700

    perbaiki ketersediaan buku

--- app/Imports/BooksImport.php
+++ app/Imports/BooksImport.php
@@ -10,14 +10,16 @@ class BooksImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
+        $eksemplar = isset($row['eksemplar']) && $row['eksemplar'] !== '' ? $row['eksemplar'] : null;
+        $ketersediaan = isset($row['ketersediaan']) && $row['ketersediaan'] !== '' ? $row['ketersediaan'] : $eksemplar;
         return new Buku([
             'judul' => $row['judul'] ?? '',
             'penulis' => $row['penulis'] ?? '',
             'penerbit' => $row['penerbit'] ?? '',
             'tahun_terbit' => isset($row['tahun_terbit']) && $row['tahun_terbit'] !== '' ? $row['tahun_terbit'] : null,
             'isbn' => $row['isbn'] ?? '',
-            'eksemplar' => isset($row['eksemplar']) && $row['eksemplar'] !== '' ? $row['eksemplar'] : null,
-            'ketersediaan' => isset($row['ketersediaan']) && $row['ketersediaan'] !== '' ? $row['ketersediaan'] : null,
+            'eksemplar' => $eksemplar,
+            'ketersediaan' => $ketersediaan,
             'no_panggil' => $row['no_panggil'] ?? '',
             'asal_koleksi' => $row['asal_koleksi'] ?? '',
             'kota_terbit' => $row['kota_terbit'] ?? '',
